952d9a8218dee5150d356f9264ef4af35cf6b132
kain <zhangtqx@vip.qq.com>
2019-01-10 12:00:20 +0800

    init:projects

---
+++ projects/ngx-ckeditor4/src/lib/ngx-ckeditor4.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-ckeditor4',
+  template: `
+    <p>
+      ngx-ckeditor4 works!
+    </p>
+  `,
+  styles: []
+})
+export class NgxCkeditor4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
