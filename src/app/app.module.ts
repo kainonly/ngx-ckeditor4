f042dc9f21100f3da74001c5bd8440f9f2ac569d
kain <kainpro@foxmail.com>
2019-01-10 12:04:13 +0800

    init:projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd';
@@ -10,17 +11,22 @@ import zh from '@angular/common/locales/zh';
 registerLocaleData(zh);
 
 import {AppComponent} from './app.component';
+import {BasicComponent} from './basic/basic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
-    NgZorroAntdModule
+    NgZorroAntdModule,
+    RouterModule.forRoot([
+      {path: '', component: BasicComponent}
+    ], {useHash: true})
   ],
   providers: [{provide: NZ_I18N, useValue: zh_CN}],
   bootstrap: [AppComponent]
