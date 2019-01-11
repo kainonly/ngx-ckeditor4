a93c92bb82333571862d95cc467c80e16006f7b9
kain <kainpro@foxmail.com>
2019-01-11 14:49:30 +0800

    develop ngx-ckeditor4

--- src/app/basic/basic.component.ts
+++ src/app/basic/basic.component.ts
@@ -9,6 +9,20 @@ export class BasicComponent {
   select = 0;
   text = '';
   config = {
+    toolbarGroups: [
+      {name: 'document', groups: ['mode', 'document', 'doctools']},
+      {name: 'clipboard', groups: ['clipboard', 'undo']},
+      {name: 'editing', groups: ['find', 'selection']},
+      {name: 'forms'},
+      {name: 'basicstyles', groups: ['basicstyles', 'cleanup']},
+      {name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi']},
+      {name: 'links'},
+      {name: 'insert'},
+      {name: 'styles'},
+      {name: 'colors'},
+      {name: 'tools'},
+      {name: 'others'},
+    ]
   };
   editorOptions = {
     theme: 'vs-light',
