aa24f7b6409c05ea33be753e6d4548683230a495
kain <kainpro@foxmail.com>
2019-01-11 18:25:04 +0800

    develop ngx-ckeditor4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  private routerEvent: Subscription;
+
+  active_path = '/';
+  routerlinks = [
+    {path: '/', label: 'Getting Started'},
+    {path: '/example', label: 'Example Ckeditor4'}
+  ];
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    this.routerEvent = this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.active_path = event.url;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.routerEvent.unsubscribe();
+  }
 }
