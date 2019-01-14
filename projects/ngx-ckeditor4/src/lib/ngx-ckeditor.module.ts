f986f025b9714ed852b99dc7c422c68c5ec4884e
kain <kainpro@foxmail.com>
2019-01-14 18:22:07 +0800

    update:lazy load module

--- projects/ngx-ckeditor4/src/lib/ngx-ckeditor.module.ts
+++ projects/ngx-ckeditor4/src/lib/ngx-ckeditor.module.ts
@@ -14,6 +14,7 @@ import {NgxCkeditorService} from './ngx-ckeditor.service';
 })
 export class NgxCkeditorModule {
   static forRoot(options: NgxCkeditorOptions): ModuleWithProviders {
+
     return {
       ngModule: NgxCkeditorModule,
       providers: [
@@ -24,6 +25,8 @@ export class NgxCkeditorModule {
   }
 
   constructor(_ngxCkeditorService: NgxCkeditorService) {
-    _ngxCkeditorService.loadScripts();
+    if (!_ngxCkeditorService.setup) {
+      _ngxCkeditorService.loadScripts();
+    }
   }
 }
