bd8c0d380cb718d4d066f284db28ef56f6fc53a1
kain <zhangtqx@vip.qq.com>
2019-01-18 17:36:18 +0800

    update:docs

--- projects/ngx-ckeditor4/src/services/options.service.ts
+++ projects/ngx-ckeditor4/src/services/options.service.ts
@@ -1,4 +1,13 @@
+import {CkeditorConfigInterface} from '../types/interface/ckeditor-config.interface';
+
 export class OptionsService {
+  /**
+   * Set the load address of ckeditor, you can use local library or remote CDN, etc.
+   */
   url: string;
-  config?: any;
+
+  /**
+   * Set the master configuration of ckeditor
+   */
+  config?: CkeditorConfigInterface;
 }
