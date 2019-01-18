6f6921cedcc7133f1a2eec53e6778751086426da
kain <zhangtqx@vip.qq.com>
2019-01-18 14:57:57 +0800

    develop: ngx-ckeditor4

--- projects/ngx-ckeditor4/src/directives/dynamic-inline.directive.ts
+++ projects/ngx-ckeditor4/src/directives/dynamic-inline.directive.ts
@@ -1,10 +1,40 @@
-import {Directive} from '@angular/core';
+import {AfterContentInit, Directive, OnDestroy} from '@angular/core';
 import {NgxCkeditorComponent} from '../component/ngx-ckeditor.component';
+import {CkeditorService} from '../services/ckeditor.service';
+import {Subscription} from 'rxjs';
 
 @Directive({
   selector: '[dynamic-inline]'
 })
-export class DynamicInlineDirective {
-  constructor(private _ngxCkeditorComponent: NgxCkeditorComponent) {
+export class DynamicInlineDirective implements AfterContentInit, OnDestroy {
+  private _inlineSubscription: Subscription;
+
+  constructor(private _ngxCkeditorComponent: NgxCkeditorComponent,
+              private _ckeditorService: CkeditorService) {
+  }
+
+  ngAfterContentInit() {
+    this._inlineSubscription = this._ckeditorService._inline.subscribe(params => {
+      if (!params.id) {
+        this.ckeditorReused(params.status);
+      } else {
+        if (params.id === this._ngxCkeditorComponent.id) {
+          this.ckeditorReused(params.status);
+        }
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this._inlineSubscription.unsubscribe();
+  }
+
+  /**
+   *  Start Reused
+   */
+  private ckeditorReused(status: boolean) {
+    this._ngxCkeditorComponent.inline = status;
+    this._ngxCkeditorComponent.reusedSubscribe();
   }
 }
+
