fa6e79cc9d8d754b5d6ec9771298a189d94729f6
kain <zhangtqx@vip.qq.com>
2019-01-20 15:49:44 +0800

    update ngx-ckeditor

--- projects/ngx-ckeditor4/src/lib/ngx-ckeditor.module.ts
+++ projects/ngx-ckeditor4/src/lib/ngx-ckeditor.module.ts
@@ -1,9 +1,7 @@
-import {ModuleWithProviders, NgModule} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {NgxCkeditorComponent} from './component/ngx-ckeditor.component';
-import {OptionsService} from './services/options.service';
-import {SetupService} from './services/setup.service';
 import {CkeditorService} from './services/ckeditor.service';
 import {DynamicConfigDirective, DynamicInlineDirective} from './directives';
 
@@ -20,26 +18,10 @@ import {DynamicConfigDirective, DynamicInlineDirective} from './directives';
     NgxCkeditorComponent,
     DynamicConfigDirective,
     DynamicInlineDirective
+  ],
+  providers: [
+    CkeditorService
   ]
 })
 export class NgxCkeditorModule {
-  /**
-   * Set Module
-   */
-  static forRoot(options: any): ModuleWithProviders {
-    return {
-      ngModule: NgxCkeditorModule,
-      providers: [
-        {provide: OptionsService, useValue: options},
-        SetupService,
-        CkeditorService
-      ],
-    };
-  }
-
-  constructor(_setupService: SetupService) {
-    if (!_setupService.setup) {
-      _setupService.loadScripts();
-    }
-  }
 }
