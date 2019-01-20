dea2619f929c97492bf1843748e9de0de12e4d9c
kain <zhangtqx@vip.qq.com>
2019-01-20 15:00:17 +0800

    update ngx-ckeditor

--- projects/ngx-ckeditor4/src/public_api.ts
+++ projects/ngx-ckeditor4/src/public_api.ts
@@ -5,5 +5,4 @@
 export * from './component/ngx-ckeditor.component';
 export * from './services/ckeditor.service';
 export * from './types';
-export * from './directives';
 export * from './ngx-ckeditor.module';
