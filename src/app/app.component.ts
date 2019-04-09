c1de9c00a6db4492ea53c5d75021a11da17b35a6
kain <kainpro@foxmail.com>
2019-04-09 16:52:08 +0800

    Submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngx-ckeditor4-dev';
+  text = '<p>asdqweqwe</p>';
+
+  change(event) {
+    console.log(event);
+  }
+
 }
