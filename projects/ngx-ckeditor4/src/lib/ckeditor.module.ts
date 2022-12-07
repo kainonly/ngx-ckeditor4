ccd32790ee0ac2cdc7181948183c8590cf1a5a0c
kain <kainpro@foxmail.com>
2022-12-07 11:42:03 +0800

    UPDATE

--- projects/ngx-ckeditor4/src/lib/ckeditor.module.ts
+++ projects/ngx-ckeditor4/src/lib/ckeditor.module.ts
@@ -5,12 +5,8 @@ import { NgxCkeditorService } from "./ckeditor.service";
 import { Option, OPTION } from "./types";
 
 @NgModule({
-  declarations: [
-    NgxCkeditorComponent
-  ],
-  exports: [
-    NgxCkeditorComponent
-  ]
+  declarations: [NgxCkeditorComponent],
+  exports: [NgxCkeditorComponent]
 })
 export class NgxCkeditorModule {
   static forRoot(option: Option): ModuleWithProviders<NgxCkeditorModule> {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {CkeditorModule} from 'ngx-ckeditor4';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {NgZorroAntdModule, NZ_I18N, en_US} from 'ng-zorro-antd';
-import {HttpClientModule} from '@angular/common/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {registerLocaleData} from '@angular/common';
-import {RouterModule} from '@angular/router';
-import en from '@angular/common/locales/en';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgxCkeditorModule } from "ngx-ckeditor4";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgZorroAntdModule, NZ_I18N, en_US } from "ng-zorro-antd";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { registerLocaleData } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import en from "@angular/common/locales/en";
 
 
 registerLocaleData(en);
 
-import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
-import {FormComponent} from './form/form.component';
-import {ConfigComponent} from './config/config.component';
-import {UploadComponent} from './upload/upload.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { FormComponent } from "./form/form.component";
+import { ConfigComponent } from "./config/config.component";
+import { UploadComponent } from "./upload/upload.component";
 
 @NgModule({
   declarations: [
@@ -24,7 +24,7 @@ import {UploadComponent} from './upload/upload.component';
     HomeComponent,
     FormComponent,
     ConfigComponent,
-    UploadComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
@@ -33,22 +33,22 @@ import {UploadComponent} from './upload/upload.component';
     NgZorroAntdModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    CkeditorModule.forRoot({
-      url: 'https://cdn.bootcss.com/ckeditor/4.11.3/ckeditor.js',
+    NgxCkeditorModule.forRoot({
+      url: "https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/ckeditor/4.17.2/ckeditor.js",
       config: {
-        filebrowserUploadMethod: 'xhr',
-        filebrowserUploadUrl: 'http://127.0.0.1:8000/index/index/uploads',
-      },
+        filebrowserUploadMethod: "xhr",
+        filebrowserUploadUrl: "http://127.0.0.1:8000/index/index/uploads"
+      }
     }),
     RouterModule.forRoot([
-      {path: '', component: HomeComponent},
-      {path: 'config', component: ConfigComponent},
-      {path: 'form', component: FormComponent},
-      {path: 'upload', component: UploadComponent},
-    ], {useHash: true})
+      { path: "", component: HomeComponent },
+      { path: "config", component: ConfigComponent },
+      { path: "form", component: FormComponent },
+      { path: "upload", component: UploadComponent }
+    ], { useHash: true })
   ],
   providers: [
-    {provide: NZ_I18N, useValue: en_US}
+    { provide: NZ_I18N, useValue: en_US }
   ],
   bootstrap: [AppComponent]
 })
