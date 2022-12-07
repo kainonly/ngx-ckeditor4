eda3e3303326714247eec3057f4990f65b016d69
kain <zhangtqx@vip.qq.com>
2022-12-07 11:42:03 +0800

    UPDATE

--- src/app/config/config.component.ts
+++ src/app/config/config.component.ts
@@ -1,18 +1,18 @@
-import {Component} from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-config',
-  templateUrl: './config.component.html',
-  styleUrls: ['./config.component.scss']
+  selector: "app-config",
+  templateUrl: "./config.component.html",
+  styleUrls: ["./config.component.scss"]
 })
 export class ConfigComponent {
   text: string;
-  locale = 'en_us';
+  locale = "en_us";
   config = {};
   basic = [
-    {name: 'document', items: ['Source', '-', 'NewPage', 'Preview', '-', 'Templates']},
-    {name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']},
-    {name: 'basicstyles', items: ['Bold', 'Italic']}
+    { name: "document", items: ["Source", "-", "NewPage", "Preview", "-", "Templates"] },
+    { name: "clipboard", items: ["Cut", "Copy", "Paste", "PasteText", "PasteFromWord", "-", "Undo", "Redo"] },
+    { name: "basicstyles", items: ["Bold", "Italic"] }
   ];
 
 }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,10 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  selector: "app-form",
+  templateUrl: "./form.component.html",
+  styleUrls: ["./form.component.scss"]
 })
 export class FormComponent implements OnInit {
   form: FormGroup;
